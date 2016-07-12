28055abb23fbffc4f2b1ec396c414d5f7ae9f714
mudkip <i@mudkip.me>
2016-07-12 19:00:46 +0800

    Basic functionality

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import Koa from 'koa';
 import path from 'path';
+import crypto from 'crypto';
 import axios from 'axios';
 import _ from 'lodash';
+import send from 'koa-send';
+import fs from 'mz/fs';
+import normalizeHeaderCase from 'header-case-normalizer';
 import config from './config.json';
+import Queue from './lib/queue';
 
 const app = new Koa();
+const queue = new Queue();
+
+const cacheRootPath = path.join(__dirname, 'cache');
+const tmpPath = path.join(cacheRootPath, 'tmp');
 
 app.use(async (ctx, next) => {
-  console.log(ctx.path);
+  // Send the cached WebP file
+  const hash = crypto.createHash('md5').update(ctx.path).digest('hex');
+  const filepath = path.join(hash.substr(0, 1), hash.substr(0, 2), hash + '.webp');
+  if (await fs.exists(path.join(cacheRootPath, filepath))) {
+    await send(ctx, filepath, {root: cacheRootPath, maxAge: 3628800 * 1000});
+    return;
+  }
+
+  if (!await fs.exists(tmpPath)) {
+    await fs.mkdir(tmpPath);
+  }
+
+  // Send the original file and convert to WebP
   const response = await axios.get(ctx.path, {
     baseURL: config.baseURL,
     headers: {'Host': config.host},
@@ -15,11 +36,23 @@ app.use(async (ctx, next) => {
   });
 
   _.each(response.headers, (value, key) => {
-    ctx.set(key, value);
+    ctx.set(normalizeHeaderCase(key), value);
   });
+
   ctx.body = response.data;
+
+  // Save the original file
+  const stream = fs.createWriteStream(path.join(tmpPath, hash));
+  response.data.pipe(stream);
+
+  stream.on('finish', function () {
+    // Create the WebP file
+    if (!queue.has(ctx.path)) {
+      queue.add(ctx.path);
+    }
+  });
 });
 
-const server = app.listen(process.env.PORT || 3002, function() {
+const server = app.listen(process.env.PORT || 3002, function () {
   console.log('Paradise server listening on port ' + server.address().port);
 });
